779e18598473b5c132db891d383602aba33fd0fc
dwb86 <dylanbenavides97@gmail.com>
2024-03-15 18:42:52 -0400

    Initial Commit

---
+++ CookBook/CookBookApp.swift
@@ -0,0 +1,20 @@
+//
+//  CookBookApp.swift
+//  CookBook
+//
+//  Created by Dylan Benavides on 3/15/24.
+//
+
+import SwiftUI
+
+@main
+struct CookBookApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
